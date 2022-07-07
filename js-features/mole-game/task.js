3878c4a7c026140e60ad83ab0e896da7c0001b0b
innavgust <innavgust@gmail.com>
2022-07-07 12:04:58 +0300

    version 2.0

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -5,29 +5,27 @@ function getHole(index) {
   return document.getElementById(`hole${index}`);
 }
 
-function deadCounter () {
-  dead.textContent -= -1;
-}
-
-function lostCounter () {
-  lost.textContent -= -1;
-}
-
-for (let i = 1; i < 10; i++) {
+function moleCounter () {
+  for (let i = 1; i < 10; i++) {
     let clickedHole = getHole(i);
     if (clickedHole.className.includes( 'hole_has-mole' )) {
-      clickedHole.onclick = deadCounter;
+      dead.textContent -= -1;
     } else {
-      clickedHole.onclick = lostCounter;
+      lost.textContent -= -1;
     }
   }
-  
-  if (lost.textContent === 5) {
-    alert('Вы проиграли!')
-    dead.textContent = 0;
-    lost.textContent = 0;
-  } else if (dead.textContent === 10) {
-    alert('Победа')
-    dead.textContent = 0;
-    lost.textContent = 0;
-  }
+}
+
+while ((lost.textContent != 5) && (dead.textContent != 10)) {
+  moleCounter ();
+}
+
+if (lost.textContent == 5) {
+  alert('Вы проиграли!')
+  dead.textContent = 0;
+  lost.textContent = 0;
+} else if (dead.textContent == 10) {
+  alert('Победа')
+  dead.textContent = 0;
+  lost.textContent = 0;
+}
