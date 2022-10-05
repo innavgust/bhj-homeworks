0e94413b056ff11efe5b1aebda7e004bba4dfb00
innavgust <innavgust@gmail.com>
2022-10-05 12:17:51 +0300

    maybe final final version

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,15 @@
 let menuElem = document.getElementsByClassName("menu__link");
 
-function menuActive() {
-    if (menuElem.item(i).nextElementSibling !== null) {
-        menuElem.item(i).nextElementSibling.classList.toggle('menu_active');
-    };
-}
+//function menuActive() {
+//    if (menuElem.item(i).nextElementSibling !== null) {
+//        menuElem.item(i).nextElementSibling.classList.toggle('menu_active');
+//    };
+//}
 
 for (let i =0; i < menuElem.length; i++) {
-    menuElem.item(i).onclick = menuActive;
-}
+    menuElem.item(i).onclick = () => {
+        if (menuElem.item(i).nextElementSibling !== null) {
+        menuElem.item(i).nextElementSibling.classList.toggle('menu_active');
+        };
+    };
+};
